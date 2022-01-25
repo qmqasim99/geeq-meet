8064b1c613b5d459d478bf36c7e1bd31c3833925
TermMC <termmc@gmail.com>
2022-01-25 11:32:59 +0000

    allow successful updates of details on user database

--- Components/UserAccount.jsx
+++ Components/UserAccount.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Text, TextInput, View, Image, StyleSheet } from "react-native";
+import { Text, TextInput, View, Image, StyleSheet, Button } from "react-native";
 import { auth } from "../firebase";
-
+import { updateEmail, updateProfile } from "firebase/auth";
 //To be careful when contacting database, user object occurs twice, once in authentication (can this be updated?) and once in Firestore database
 
 const UserAccount = () => {
@@ -12,27 +12,45 @@ const UserAccount = () => {
     avatar: auth.currentUser.photoURL,
     transport: "car", //would have to get transport method from database here
   });
+  const [email, setEmail] = useState(user.email);
+  const [name, setName] = useState(user.name);
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [transport, setTransport] = useState(user.transport);
 
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Email</Text>
-      <TextInput value={"this will be your emails"} style={styles.input} />
-      {/* is it possible to update original user object used for identification, i.e. email address */}
+      <TextInput
+        value={user.email}
+        style={styles.input}
+        onChangeText={(text) => {
+          setUser((prevUser) => {
+            return { ...prevUser, email: text };
+          });
+        }}
+      />
+
       <Text style={styles.heading}>Name</Text>
       <TextInput
         value={user.name}
+        placeholder={"Update Me!"}
+        styles={styles.input}
         onChangeText={(text) => {
           setUser((prevUser) => {
             return { ...prevUser, name: text };
           });
         }}
-        styles={styles.input}
       />
 
       <Text style={styles.heading}>Avatar</Text>
-      <Image src={user.avatar} />
+      {user.avatar ? (
+        <Image source={{ uri: user.avatar }} style={styles.avatar} />
+      ) : (
+        <Text>No Avatar Currently</Text>
+      )}
       <TextInput
         value={user.avatar}
+        placeholder={"Update Me!"}
         onChangeText={(text) => {
           setUser((prevUser) => {
             return { ...prevUser, avatar: text };
@@ -42,6 +60,7 @@ const UserAccount = () => {
       />
       <Text style={styles.heading}>Default Transport</Text>
       <TextInput
+        placeholder={"Update Me!"}
         value={user.transport}
         onChangeText={(text) => {
           setUser((prevUser) => {
@@ -49,6 +68,23 @@ const UserAccount = () => {
           });
         }}
         style={styles.input}
+      />
+      <Button
+        title="Submit"
+        onPress={() => {
+          updateProfile(auth.currentUser, {
+            displayName: user.name,
+            photoURL: user.avatar,
+          })
+            .then(
+              updateEmail(auth.currentUser, user.email)
+                .then(() => alert("Details updated")) //display alert saying details updated?
+                .catch((error) => {
+                  alert(error.message);
+                })
+            )
+            .catch((error) => alert(error.message));
+        }}
       />
     </View>
   );
@@ -72,5 +108,10 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(255, 255, 255, 0.5)",
     fontSize: 18,
     minWidth: "80%",
+    flexWrap: "wrap",
+  },
+  avatar: {
+    height: 100,
+    width: 100,
   },
 });
