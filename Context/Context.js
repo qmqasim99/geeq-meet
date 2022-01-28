4033db9873c8dd35708239dd4b41df0f6d1e4730
ed <edheyev@gmail.com>
2022-01-28 10:26:34 +0000

    context added currentgroupstate

--- Context/Context.js
+++ Context/Context.js
@@ -4,6 +4,11 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState({});
+  const [currentGroup, setCurrentGroup] = useState({});
+
+  useEffect(() => {
+    ApiCallToUpdateUser();
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
