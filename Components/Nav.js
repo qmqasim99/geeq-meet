b14408a595e6752ae6a94c3efeaf557284447961
qmqasim99 <Qmqasim99@gmail.com>
2022-01-31 15:05:40 +0000

    fixed create group and showing avatar

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,37 +1,38 @@
-import React, { useState } from "react";
-import { ButtonGroup } from "react-native-elements";
-import { Text, StyleSheet } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { useRoute } from "@react-navigation/native";
+import React, { useState } from 'react';
+import { ButtonGroup } from 'react-native-elements';
+import { Text, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
 
 const Nav = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const navigation = useNavigation();
 
+  const route = useRoute();
+
   const handleNavigate = (value) => {
     // import { useRoute } from "@react-navigation/native";
 
-    const route = useRoute();
     console.log(route);
 
-    if (value === 1) {
-      navigation.navigate("Home");
+    if (value === 0) {
+      navigation.navigate('Home');
     }
     if (value === 1) {
-      navigation.navigate("Chat_2");
+      navigation.navigate('Chat_2');
     }
     if (value === 2) {
-      navigation.navigate("ConfigureMeet");
+      navigation.navigate('ConfigureMeet');
     }
     if (value === 3) {
-      navigation.navigate("UserAccount");
+      navigation.navigate('UserAccount');
     }
   };
 
   return (
     <>
       <ButtonGroup
-        buttons={["Home", "Chat", "Meet", "UserAccount"]}
+        buttons={['Home', 'Chat', 'Meet', 'UserAccount']}
         selectedIndex={selectedIndex}
         onPress={(value) => {
           handleNavigate(value);
@@ -44,9 +45,9 @@ const Nav = () => {
 
 const styles = StyleSheet.create({
   subHeader: {
-    backgroundColor: "#2089dc",
-    color: "white",
-    textAlign: "center",
+    backgroundColor: '#2089dc',
+    color: 'white',
+    textAlign: 'center',
     paddingVertical: 5,
     marginBottom: 10,
   },
