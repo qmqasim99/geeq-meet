967728e0234a2bca0b3206dadfbcad8baac76a03
ed <edheyev@gmail.com>
2022-01-31 15:32:24 +0000

    checking out  main

--- screens/ConfigureMeet.js
+++ screens/ConfigureMeet.js
@@ -1,11 +1,29 @@
 import { StyleSheet, Text, Picker, View, TouchableOpacity } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import CheckboxList from "rn-checkbox-list";
+import { UserContext, ThemeContext } from "../Context/Context";
 
 export default function ConfigureMeet() {
   const navigation = useNavigation();
+  const theme = useContext(ThemeContext);
+  const { currentGroup, user } = useContext(UserContext);
+
   const [selectedValue, setSelectedValue] = useState("java");
   const meetTypeList = ["cafe", "restaurant", "cinema", "park", "pub"];
+  let memberAr = [];
+  // useEffect(() => {
+  //   //onload create array for checkbox list
+  //   memberAr = currentGroup.user.map((user, i) => {
+  //     const member = { ...user };
+  //     member.id = i;
+  //     return member;
+  //   });
+  // }, [currentGroup]);
+
+  const handleConfigureMeet = (e) => {
+    navigation.navigate("MapContainer");
+  };
 
   return (
     <View>
@@ -19,12 +37,21 @@ export default function ConfigureMeet() {
           return <Picker.Item key={i} label={type} value={type} />;
         })}
       </Picker>
+      {/* <CheckboxList
+        headerName="Movies"
+        theme="red"
+        listItems={data}
+        onChange={({ ids, items }) => console.log("My updated list :: ", ids)}
+        listItemStyle={{ borderBottomColor: "#eee", borderBottomWidth: 1 }}
+        checkboxProp={{ boxType: "square" }} // iOS (supported from v0.3.0)
+        onLoading={() => <LoaderComponent />}
+      /> */}
       <TouchableOpacity
         onPress={(e) => {
-          navigation.navigate("MapContainer");
+          handleConfigureMeet;
         }}
       >
-        <Text>go to maps</Text>
+        <Text>Let's Meet!</Text>
       </TouchableOpacity>
     </View>
   );
